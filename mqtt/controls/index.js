d654fc452fcb38697ffdfd6b8ec20a63ccd0b598
cadenzah <cadenzah93@gmail.com>
2019-06-09 01:03:46 +0900

    v0.2.2 - added mqtt publish handler codes for eventCode 1~7   (mqtt/controls/index.js) - currently test codes for pub handler are included in mqtt/index.js

--- mqtt/controls/index.js
+++ mqtt/controls/index.js
@@ -1,17 +1,9 @@
-// gameId와 event를 받고
-// event에 따라 Mongoose 모델 조작
-// 조작 완료 후 달라진 내용을 publish
-  // /update/gameId /w { event }
-
-// 조작할 것: gameId에 대응하는 game model
 // signature: (client: MQTT_instance, gameId: String, event: Number)
 const Game = require('../../schemas/game')
 const controller = (client, gameId, eventCode) => {
-  console.log('controller')
-  console.log(eventCode)
   switch (eventCode) {
-    case 0:
-      Game.findByIdAndUpdate({ _id: gameId }, { state: 0 }, null, (err) => {
+    case 0: // device connected
+      Game.findByIdAndUpdate({ _id: gameId }, { state: 'device_ready' }, null, (err) => {
         if (err) { // 업데이트에 실패하였음을 publish
           console.log('fail')
           client.publish(`update/${gameId}`, JSON.stringify({
@@ -21,36 +13,157 @@ const controller = (client, gameId, eventCode) => {
         } else { // 정상 변경되었음을 publish
           console.log('success')
           client.publish(`/update/${gameId}`, JSON.stringify({
-            msg: 'update successful',
+            msg: 'device_ready',
             event: 0
           }))
         }
       })
       break
 
-    case 1:
+    case 1: // game started
+      Game.findByIdAndUpdate({ _id: gameId }, { state: 'in_progress' }, null, (err) => {
+        if (err) { // 업데이트에 실패하였음을 publish
+          console.log('fail')
+          client.publish(`update/${gameId}`, JSON.stringify({
+            msg: 'update failed',
+            event: -1
+          }))
+        } else { // 정상 변경되었음을 publish
+          console.log('success')
+          client.publish(`/update/${gameId}`, JSON.stringify({
+            msg: 'in_progress',
+            event: 1
+          }))
+        }
+      })
       break
 
-    case 2:
+    case 2: // game done
+      Game.findByIdAndUpdate({ _id: gameId }, { state: 'game_over' }, null, (err) => {
+        if (err) { // 업데이트에 실패하였음을 publish
+          console.log('fail')
+          client.publish(`update/${gameId}`, JSON.stringify({
+            msg: 'update failed',
+            event: -1
+          }))
+        } else { // 정상 변경되었음을 publish
+          console.log('success')
+          client.publish(`/update/${gameId}`, JSON.stringify({
+            msg: 'game_over',
+            event: 2
+          }))
+        }
+      })
       break
 
-    case 3:
+    case 3: // team A scored
+      Game.findByIdAndUpdate({ _id: gameId },
+        { $inc: { 'team_A.score': 1 } },
+        { new: true },
+        (err, game) => {
+        if (err) { // 업데이트에 실패하였음을 publish
+          console.log('fail')
+          client.publish(`update/${gameId}`, JSON.stringify({
+            msg: 'update failed',
+            event: -1
+          }))
+        } else { // 정상 변경되었음을 publish
+          console.log('success')
+          client.publish(`/update/${gameId}`, JSON.stringify({
+            msg: `team A scored - ${game.team_A.score}:${game.team_B.score}`,
+            event: 3
+          }))
+        }
+      })
       break
 
-    case 4:
+    case 4: // team B scored
+      Game.findByIdAndUpdate({ _id: gameId },
+        { $inc: { 'team_B.score': 1 } },
+        { new: true },
+        (err, game) => {
+        if (err) { // 업데이트에 실패하였음을 publish
+          console.log('fail')
+          client.publish(`update/${gameId}`, JSON.stringify({
+            msg: 'update failed',
+            event: -1
+          }))
+        } else { // 정상 변경되었음을 publish
+          console.log('success')
+          client.publish(`/update/${gameId}`, JSON.stringify({
+            msg: `team B scored - ${game.team_A.score}:${game.team_B.score}`,
+            event: 4
+          }))
+        }
+      })
       break
 
-    case 5:
+    case 5: // team A deducted
+      Game.findByIdAndUpdate({ _id: gameId },
+        { $inc: { 'team_A.score': -1 } },
+        { new: true },
+        (err, game) => {
+        if (err) { // 업데이트에 실패하였음을 publish
+          console.log('fail')
+          client.publish(`update/${gameId}`, JSON.stringify({
+            msg: 'update failed',
+            event: -1
+          }))
+        } else { // 정상 변경되었음을 publish
+          console.log('success')
+          client.publish(`/update/${gameId}`, JSON.stringify({
+            msg: `team A deducted - ${game.team_A.score}:${game.team_B.score}`,
+            event: 5
+          }))
+        }
+      })
       break
 
-    case 6:
+    case 6: // team B deducted
+      Game.findByIdAndUpdate({ _id: gameId },
+        { $inc: { 'team_B.score': -1 } },
+        { new: true },
+        (err, game) => {
+        if (err) { // 업데이트에 실패하였음을 publish
+          console.log('fail')
+          client.publish(`update/${gameId}`, JSON.stringify({
+            msg: 'update failed',
+            event: -1
+          }))
+        } else { // 정상 변경되었음을 publish
+          console.log('success')
+          client.publish(`/update/${gameId}`, JSON.stringify({
+            msg: `team B deducted - ${game.team_A.score}:${game.team_B.score}`,
+            event: 6
+          }))
+        }
+      })
       break
 
-    case 7:
+    case 7: // other cases i.e. player wound, no shuttlecock, player not shown...
+      Game.findByIdAndUpdate({ _id: gameId }, { state: 'problem' }, null, (err) => {
+        if (err) { // 업데이트에 실패하였음을 publish
+          console.log('fail')
+          client.publish(`update/${gameId}`, JSON.stringify({
+            msg: 'update failed',
+            event: -1
+          }))
+        } else { // 정상 변경되었음을 publish
+          console.log('success')
+          client.publish(`/update/${gameId}`, JSON.stringify({
+            msg: 'problem occured',
+            event: 7
+          }))
+        }
+      })
       break
 
-    default:
-
+    default: // event code is out of range; return error message
+      client.publish(`/update/${gameId}`, JSON.stringify({
+        msg: 'update failed - wrong event code (1 ~ 7)',
+        event: eventCode
+      }))
+      break
   }
 }
 
--- mqtt/index.js
+++ mqtt/index.js
@@ -6,39 +6,27 @@ const mqtt = (app) => {
 
   client.on('connect', (connack) => {
     console.log(`MQTT Broker와 연결 성공`)
-    // 처음 디바이스를 등록했을 때에 해당 디바이스에 대한 고유 번호를 얻어올 필요가 있다.
-    // 그래야 위변조에 대한 대책을 세울 수 있을 듯
-    // 아니면, 패스워드가 일치해야만 하도록?
 
-    // 정보: 디바이스에서는 버튼 이외의 다른 프로그래밍적 조작이 불가
-
-    // 결론: 어차피 악의의 디바이스로부터 조작이 있다고 하면
-    // 바로 현장의 사람들에 의하여 들통나므로
-    // 딱히 그럴 필요는 없어보임
-    // 단, REST 클라이언트에 의한 수정은 검열해야할듯
-
+    // test codes to simulate event from device
     client.subscribe('/event/#', (err) => {
-      // if (!err) client.publish('/123/34/dd/sss', JSON.stringify({
-      //   msg: 'hi!'
-      // }))
       client.publish('/event/5ce78a7de73c2f3660a992e8', JSON.stringify({
-        eventCode: 0
+        eventCode: 13
       }))
     })
+
+    // test codes to subscribe the update made by server
+    client.subscribe('/update/#')
   })
 
   client.on('message', (topic, message) => {
-    // 토픽에 들어있는 경기 id를 기반으로 데이터 수정
-    // const topicPattern = '/+competId/+gameId/#'
-    // const topics = mqttParse.exec(topicPattern, topic)
-    // console.dir(topics)
-    // console.dir(JSON.parse(message.toString()))
     const topicPattern = '/event/+gameId/#'
     const topics = mqttParse.exec(topicPattern, topic)
 
-    if (Object.keys(topics).includes('gameId')) {
+    if (!!topics && Object.keys(topics).includes('gameId')) {
       const { eventCode } = JSON.parse(message.toString())
       if (eventCode !== undefined) controller(client, topics.gameId, eventCode)
+    } else {
+      console.log(JSON.parse(message.toString()))
     }
   })
   // save the reference of this mqtt client
